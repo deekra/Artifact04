e79d2590670add7625bcb52978b11087cbb2c259
deekra <114116356+deekra@users.noreply.github.com>
2022-11-28 23:57:00 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-function
+function validateForm(){
+ var validFirstName=false;
+ var letters = /^[A-Za-z]+$/;
+ var validLastName=false;
+ var errorMessages ="";
+ 
+  
+  if (FirstName==="null" || FirstName==="" || FirstName.length > 20)
+    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
+  else
+   validFirstName = true;
