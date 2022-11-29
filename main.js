33538c730ecb1b67ec3d9cf3206a870c065cb76c
deekra <114116356+deekra@users.noreply.github.com>
2022-11-29 11:43:26 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function validateForm(){
- var validFirstName=false;
+ var validFirstname = false;
  var letters = /^[A-Za-z]+$/;
- var validLastName=false;
+ var validLastname = false;
  var errorMessages ="";
  var validEmail = false;
  var validPhone = false;
@@ -12,24 +12,63 @@ function validateForm(){
  var validCity = false;
  var validState = false;
  var validCountry = false;
- var validZipCode = false;
+ var validZipcode = false;
   
  
-  var FirstName = document.getElementById("FirstName").value;
-  var validLastName = document.getElementById("LastName").value;
+  var Firstname = document.getElementById("Firstname").value;
+  var validLastname = document.getElementById("Lastname").value;
+  var validEmail = document.getElementById("Email").value;
+  var validPhone = document.getElementById("Phone").value;
+  var validUsername = document.getElementById("Username").value;
+  var validPassword = document.getElementById("Password").value;
+  var validAddress = document.getElementById("Address").value;
+  var validCity = document.getElementById("City").value;
+  var validState = document.getElementById("State").value;
+  var validCountry = document.getElementById("Country").value;
+  var validZipcode = document.getElementById("Zipcode").value;
 
-  if (FirstName==="null" || FirstName==="" ||  FirstName.value.match(letters) ||  FirstName.length > 20 ||  FirstName.length < 0)
-    errorMessages += "<p>The First Name is required, only letters are accepted and cannot be greater than 20 characters</p>";
+  if (Firstname==="null" || Firstname==="" ||  Firstname.value.match(letters) ||  Firstname.length > 20 ||  Firstname.length < 0)
+    errorMessages += "<p>The First Name is required, only letters are accepted and it cannot be greater than 20 characters</p>";
   else
    validFirstName = true;
  
-  if (LastName==="null" || LastName==="" ||  LastName.value.match(letters) || LastName.length > 50)
-    errorMessages += "<p>The Last Name is required and cannot be greater than 50 characters</p>";
+  if (Lastname==="null" || Lastname==="" ||  LastName.value.match(letters) || Lastname.length > 50)
+    errorMessages += "<p>The Last Name is required, only letters are accepted and it cannot be greater than 50 characters</p>";
+  else
+   validLastname = true;
+ 
+ var atpos = userEmail.indexOf("@");
+ var dotpos = userEmail.lastIndexOf(".");
+ if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length)
+  
+  if (isNaN(Phone) || Phone.length >15 || Phone===null || Phone==="" || Phone.value.match(numbers) )
+  For example errorMessages = "<p>Invalid phone number </p>";
+else
+  return true;
+ 
+  if (Username==="null" || Username==="" ||  Username.length >12 ||  Username.length < 0)
+    errorMessages += "<p>The Username is required and cannot be greater than 12 characters</p>";
   else
    validFirstName = true;
  
+  if (Password==="null" || Password==="" || Password.length > 7 || Password.length < 0)
+    errorMessages += "<p>The Password is required and cannot be greater than 7 characters</p>";
+  else
+   validFirstName = true;
+ 
+ if (Address==="null" ||  Address==="" || Address.length < 0)
+    errorMessages += "<p>The Address is required </p>";
+  else
+   validFirstName = true;
+ 
+ if (City==="null" || City==="" || City.length < 0)
+    errorMessages += "<p>The City is required </p>";
+  else
+   validFirstName = true;
  
  
  document.getElementById("errorMessages").innerHTML = errorMessages;
  
- return (validFirstName);
+ return (validFirstname);
+ return (validLastname);
+
