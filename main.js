552263b1f8dd4e6eee83e1e44b8b00b9c7aff5b6
deekra <114116356+deekra@users.noreply.github.com>
2022-11-29 10:18:18 -0800

    Update main.js

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function validateForm(){
   var FirstName = document.getElementById("FirstName").value;
   var validLastName = document.getElementById("LastName").value;
 
-  if (FirstName==="null" || FirstName==="" ||  FirstName.length > 20)
-    errorMessages += "<p>The First Name is required and cannot be greater than 20 characters</p>";
+  if (FirstName==="null" || FirstName==="" ||  FirstName.value.match(letters) ||  FirstName.length > 20)
+    errorMessages += "<p>The First Name is required, only letters are accepted and cannot be greater than 20 characters</p>";
   else
    validFirstName = true;
  
-  if (LastName==="null" || LastName==="" || LastName.length > 50)
+  if (LastName==="null" || LastName==="" ||  LastName.value.match(letters) || LastName.length > 50)
     errorMessages += "<p>The Last Name is required and cannot be greater than 50 characters</p>";
   else
    validFirstName = true;
